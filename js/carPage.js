0651f5a62981136e418cc6b23e0fd21d618c2835
AravindSakthivel <aravind.sakthi@outlook.com>
2020-08-13 09:47:07 +0530

    Added: user can rent car

--- js/carPage.js
+++ js/carPage.js
@@ -1,8 +1,26 @@
 let currentUser = lgdUser.allData()
+let footer = document.getElementById('ftr')
+let charge
+
 window.onload = () => {
     loadCarPage()
+    let selDays = document.getElementById('no_of_days')
+    selDays.addEventListener('change', () =>{
+        getTotal(event.target.value)
+    })
 }
 
+
+const getTotal = (value) =>{
+    console.log(value)
+    let finalValue = document.getElementById('final_amount')
+    setTimeout(() => {
+        finalValue.textContent = ""
+        finalValue.textContent ='Total: ' + Number(charge) * Number(value)
+        footer.style.display = ''
+    }, 500)
+    
+}
 
 const loadCarPage = () =>{
     let query = window.location.search
@@ -16,11 +34,10 @@ const loadCarPage = () =>{
         let whoseCar = new UserDataBase(allCarInfo[selCar].Name)
         let whoseCarAllData = whoseCar.allData()
         let carInfo = whoseCarAllData.listed[allCarInfo[selCar].carNo]
+        charge = carInfo.Charges
+        console.log(charge)
         let carOwnername = allCarInfo[selCar].FullName
-        let requestBtnHld = document.getElementById('request_btn_hld')
-        requestBtnHld.innerHTML += `
-            <button type="button" class="btn btn-dark float-right" id="request_btn">Request</button>
-        `
+        let estimateHld = document.getElementById('estimate_hld')
         let requestBtn = document.getElementById('request_btn')
         requestBtn.addEventListener('click', () => {
             requestCar(whoseCar, allCarInfo[selCar].carNo, allCarInfo, selCar)
@@ -30,7 +47,22 @@ const loadCarPage = () =>{
 
 
 const requestCar = (whoseCar, id, carLedger, car) =>{
-    whoseCar.carRented(id, currentUser.UserName)
-    let crnUserDataBase = new UserDataBase(currentUser.UserName)
-    crnUserDataBase.carRequested(carLedger[car])
+    // whoseCar.carRented(id, currentUser.UserName)
+    // let crnUserDataBase = new UserDataBase(currentUser.UserName)
+    // crnUserDataBase.carRequested(carLedger[car])
+    let finalMsg = document.getElementById('estimate_hld')
+    footer.style.display = 'none'
+    finalMsg.innerHTML = ""
+    finalMsg.innerHTML +=`
+        <div class="spinner-grow" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>`
+    setTimeout(()=>{
+        finalMsg.innerHTML = ""
+        finalMsg.innerText = 'Success'
+        let rentBtn = document.getElementById('rent_btn')
+        rentBtn.textContent = 'Rented'
+        rentBtn.disabled = true
+    }, 1000)
+    
 }
