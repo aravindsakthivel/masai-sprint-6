7e268f14879dd8af9936b160b0c39bc31242a5c0
AravindSakthivel <aravind.sakthi@outlook.com>
2020-08-13 08:13:14 +0530

    Added: user can request car

--- js/carPage.js
+++ js/carPage.js
@@ -1,25 +1,36 @@
+let currentUser = lgdUser.allData()
 window.onload = () => {
-    loadCar()
+    loadCarPage()
 }
 
 
-const loadCar = () =>{
+const loadCarPage = () =>{
     let query = window.location.search
     let url = new URLSearchParams(query)
-    let carNo = (url.get('q'))
-    if(carNo === null){
+    let selCar = (url.get('q'))
+    if(selCar === null){
         window.location.replace('../html/login.html')
     }
     else{
-        let allUserInfo = carLedger.allData()
-        let whoseCar = new UserDataBase(allUserInfo[carNo].Name)
+        let allCarInfo = carLedger.allData()
+        let whoseCar = new UserDataBase(allCarInfo[selCar].Name)
         let whoseCarAllData = whoseCar.allData()
-        let carInfo = whoseCarAllData.listed[allUserInfo[carNo].carNo]
-        // renderDom(carInfo, allUserInfo[carNo].FullName)
+        let carInfo = whoseCarAllData.listed[allCarInfo[selCar].carNo]
+        let carOwnername = allCarInfo[selCar].FullName
+        let requestBtnHld = document.getElementById('request_btn_hld')
+        requestBtnHld.innerHTML += `
+            <button type="button" class="btn btn-dark float-right" id="request_btn">Request</button>
+        `
+        let requestBtn = document.getElementById('request_btn')
+        requestBtn.addEventListener('click', () => {
+            requestCar(whoseCar, allCarInfo[selCar].carNo, allCarInfo, selCar)
+        })
     }
 }
 
 
-const renderDom = (carData, name) => {
-    console.log(carData, name)
+const requestCar = (whoseCar, id, carLedger, car) =>{
+    whoseCar.carRented(id, currentUser.UserName)
+    let crnUserDataBase = new UserDataBase(currentUser.UserName)
+    crnUserDataBase.carRequested(carLedger[car])
 }
